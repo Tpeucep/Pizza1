588ea9ece3784664258d4a772f18d8ddf96f3770
xanya <Xanya47@inbox.lv>
2024-04-21 18:39:08 +0300

    add to vscode

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const ButtonDiv = styled.div`
-display: flex; 
-height: 7vh;
-top: 0;
-justify-content: center;
-align-items: center;
-background: grey;
-`
+    display: flex;
+    height: 7vh;
+    top: 0;
+    justify-content: center;
+    align-items: center;
+    background: grey;
+`;
 
 const GreenButton = styled.button`
-background: green;
-`
+    background: green;
+`;
 
-export const App = () =>{
-
+export const App = () => {
     const [count, setCount] = React.useState(0);
 
-    return( 
-    <div>  
-        <ButtonDiv>{count} times clicked  </ButtonDiv>
-        <GreenButton onClick={()=>setCount(count+1)}> click  </GreenButton>
-    </div>)
-}
+    return (
+        <div>
+            <ButtonDiv>{count} times clicked </ButtonDiv>
+            <GreenButton onClick={() => setCount(count + 1)}> click </GreenButton>
+        </div>
+    );
+};
