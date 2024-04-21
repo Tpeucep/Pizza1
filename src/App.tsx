9e62f756d8bb9d0855d04468ab01890a712dc139
xanya <Xanya47@inbox.lv>
2024-04-21 18:14:12 +0300

    add styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React from "react"
+import React from "react";
+import styled from 'styled-components';
+
+const GreenButton = styled.button`
+background: green;
+`
 
 export const App = () =>{
 
     const [count, setCount] = React.useState(0);
 
-    return( <div> APP 
-        <button onClick={()=>setCount(count+1)}> time {count} </button>
+    return( <div>  
+        <GreenButton onClick={()=>setCount(count+1)}> time {count} </GreenButton>
     </div>)
 }
